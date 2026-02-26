d1c840dea289e5f1358e3bf50c3be1e041f96ae0
ferhany <f.yildiz2002@gmail.com>
2026-02-26 09:08:53 +0100

    Add WHITELISTED_EMAILS environment variable

--- apps/admin-server/src/lib/server-side-props-definition.ts
+++ apps/admin-server/src/lib/server-side-props-definition.ts
@@ -1,7 +1,22 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
+
 export const withApiUrl = (async () => {
   return { props: { apiUrl: process.env.API_URL } };
 }) satisfies GetServerSideProps<{ apiUrl: string }>;
 
 export type WithApiUrlProps = InferGetServerSidePropsType<typeof withApiUrl>;
+
+export const withWhitelistedEmails = (async () => {
+  const whitelistedEmails = (process.env.WHITELISTED_EMAILS ?? '')
+    .split(/[\n,]/)
+    .map((e) => e.trim())
+    .filter(Boolean);
+  console.log('WHITELISTED_EMAILS raw:', process.env.WHITELISTED_EMAILS);
+  console.log('whitelistedEmails parsed:', whitelistedEmails);
+  return { props: { whitelistedEmails } };
+}) satisfies GetServerSideProps<{ whitelistedEmails: string[] }>;
+
+export type WithWhitelistedEmailsProps = InferGetServerSidePropsType<
+  typeof withWhitelistedEmails
+>;
